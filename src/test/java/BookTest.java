57865f64d172a81702313465cbded33d01a4d73b
AgRafis <120460384+AgRafis@users.noreply.github.com>
2023-03-12 15:49:05 +0500

    Update BookTest.java

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,15 +1,61 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.library.Book;
 import ru.library.Category;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 public class BookTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
     @Test
-    public void shouldSetCategory() {
+    public void shouldSetCategoryNotNull() {
         final Book book = new Book("bookName", "bookAuthor");
         book.setCategory(new Category("categoryName"));
+        if (book.getCategory() != null) {
+            Assertions.assertNotNull(book.getCategory());
+        }
+        else {
+            System.out.println("Не получилось привязать категорию, категория не содержит имени");
+            Assertions.assertEquals("Не получилось привязать категорию, категория не содержит" +
+                    " имени", outputStreamCaptor.toString().trim());
+        }
 
-        Assertions.assertNotNull(book.getCategory());
+    }
+    @Test
+    public void shouldSetCategoryNull() {
+        Book book = new Book("bookName", "bookAuthor");
+        book.setCategory(new Category("categoryName"));
+        Category a = book.getCategory();
+        a = null;
+        if (a != null) {
+            Assertions.assertNotNull(book.getCategory());
+        }
+        else {
+            System.out.println("Не получилось привязать категорию, категория не содержит имени");
+            Assertions.assertEquals("Не получилось привязать категорию, категория не содержит" +
+                    " имени", outputStreamCaptor.toString().trim());
+        }
+
+    }
+    @Test
+    public void shouldGetName(){
+        Book book = new Book("bookName", "bookAuthor");
+        String res = book.getName();
+
+        Assertions.assertEquals("bookName", res);
+    }
+    @Test
+    public void shouldGetAuthor(){
+        Book book = new Book("bookName", "bookAuthor");
+        String res = book.getAuthor();
+
+        Assertions.assertEquals("bookAuthor", res);
     }
 }
